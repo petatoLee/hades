6927093cf840d12f86943dc6322aef3011b5b918
liming1 <1270218557@qqq>
2018-05-02 21:24:51 +0800

    小猴子

--- src/main/java/com/lee/learning/java8/MainTest.java
+++ src/main/java/com/lee/learning/java8/MainTest.java
@@ -1,10 +1,15 @@
 package com.lee.learning.java8;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainTest {
 	public static void main(String[] args) {
-		//test sorting
 		System.out.println(Integer.compare(5, 4));
-		//Double.compare(arg0, arg1)
+		List<String> list=Arrays.asList("123","44444","44","3");
+		Arrays.sort(list, (String s1,String s2)-> Integer.compare(s1.length(),s2.length()));
+		
+		
 	}
 
 }
